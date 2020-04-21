f6c7ee584b0f9f2144ecbc7fb0c13707d6dbed4b
Diyue Gu <diyueg@uci.edu>
2020-04-20 21:02:17 -0700

    Setup Superhero Collection View

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Ivy on 4/20/20.
+//  Copyright © 2020 ivygdy. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
