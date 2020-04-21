3c02dc9e023f76d68d5a33e74c81fbe819a252f1
Diyue Gu <diyueg@uci.edu>
2020-04-20 18:42:31 -0700

    Add navigation and pass data

---
+++ flix/MovieDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetailsViewController.swift
+//  flix
+//
+//  Created by Ivy on 4/20/20.
+//  Copyright © 2020 ivygdy. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    var movie: [String:Any]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
